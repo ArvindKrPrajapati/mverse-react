0d90d0cfb8adf838dae74a749663ac58d2b7b8e0
Arvind Kumar <arvind@earnnest.me>
2023-05-30 14:19:57 +0530

    Revert "added star for rating"
    This reverts commit 893acd60f246a0fcf5356b7157147451c1a05d0f.

--- src/pages/Play/index.js
+++ src/pages/Play/index.js
@@ -18,7 +18,6 @@ import Loading from "../../components/Loading";
 import Player from "../../components/Player";
 import Footer from "../../components/Footer";
 import { BsChevronRight } from "react-icons/bs";
-import BeautyStars from "beauty-stars";
 
 export default function Play({ tv }) {
   const location = useLocation();
@@ -30,7 +29,6 @@ export default function Play({ tv }) {
   const [recomendation, setRecomendation] = useState();
   const [similar, setSimilar] = useState();
   const [loading, setLoading] = useState(true);
-  const [star, setStar] = useState(0);
 
   const _init = async () => {
     try {
@@ -90,21 +88,11 @@ export default function Play({ tv }) {
         </div>
         <div className="d-flex p-3">
           <div className="d-none d-md-block">
-            <div style={{ position: "sticky", top: "20%" }}>
-              <img
-                src={imageUrl + "w300" + data.poster_path}
-                className="poster_img"
-                alt="img"
-              />
-              <br />
-              <div className="p-2">
-                <b className="text-white">Rate this</b>
-                <BeautyStars
-                  value={star}
-                  onChange={(value) => setStar(value)}
-                />
-              </div>
-            </div>
+            <img
+              src={imageUrl + "w300" + data.poster_path}
+              className="poster_img"
+              alt="img"
+            />
           </div>
           <div className="details">
             <b className="title d-block">{data.title || data.name}</b>
